3b2dd887836019d3b3f0a0da0a779eb4ccce1183
georgebrown-0 <georgeabrown14@gmail.com>
2025-12-08 18:54:23 +0000

    Update plotting.py

--- utilities/plotting.py
+++ utilities/plotting.py
@@ -7,12 +7,12 @@ def plot_prices(prices, t1, t2):
     fig.update_layout(title="Price History", xaxis_title="Date", yaxis_title="Price")
     return fig
 
-def plot_spread_zscore(spread, zs):
+def plot_spread_zscore(spread, zs, entry_z = 2.0):
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=spread.index, y=spread, name="Spread"))
     fig.add_trace(go.Scatter(x=zs.index, y=zs, name="Z-Score"))
-    fig.add_hline(y=2, line_dash="dash")
-    fig.add_hline(y=-2, line_dash="dash")
+    fig.add_hline(y=entry_z, line_dash="dash")
+    fig.add_hline(y=-entry_z, line_dash="dash")
     fig.update_layout(title="Spread & Z-Score")
     return fig
 
